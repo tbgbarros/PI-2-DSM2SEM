6d945722736579290a9de6dae833f52bfa824cb6
Kauan Bomfim <111811804+wkauan@users.noreply.github.com>
2023-05-29 21:26:31 -0300

    Atualizado

---
+++ BANCO DE DADOS/prontuarioBD.sql
@@ -0,0 +1,41 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.13-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para prontuariobd
+CREATE DATABASE IF NOT EXISTS `prontuariobd` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `prontuariobd`;
+
+-- Copiando estrutura para tabela prontuariobd.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `ID_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `ID_medico` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_usuario`),
+  UNIQUE KEY `usuario` (`usuario`),
+  UNIQUE KEY `senha` (`senha`),
+  KEY `fk_ID_medico` (`ID_medico`),
+  CONSTRAINT `fk_ID_medico` FOREIGN KEY (`ID_medico`) REFERENCES `medico` (`ID_medico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
