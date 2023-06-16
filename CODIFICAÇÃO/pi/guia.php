0f10fa34cc504f84613ad114fd23e881dd1b001b
tbgbarros <tbg.barros@hotmail.com>
2023-06-16 07:56:23 -0300

    novo layout de restorno consulta

--- CODIFICAÇÃO/pi/guia.php
+++ CODIFICAÇÃO/pi/guia.php
@@ -235,209 +235,35 @@ if (isset($_POST['editar'])) {
           <div class="container">
             <hr>
             <div class="table-responsive">
-              <table class="table table-sm table-bordered table-hover">
-
-                <div class="col-lg-8 d-flex align-items-stretch">
-                  <div class="card w-100">
-                    <div class="card-body p-4">
-                      <h5 class="card-title fw-semibold mb-4"> Consultas Recentes</h5>
-                      <div class="table-responsive">
-                        <table class="table  mb-0 align-middle">
-                          <thead class="text-dark fs-4">
-                            <tr>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Paciente</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 fw-semibold'>CPF</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Sexo</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Fone Paciente</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Data consulta</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Nome Medico</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Especialização</span></h6>
-                              </th>
-                              <th class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Hospital</span></h6>
-                              </th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <?php
-                              require_once 'class/func.php';
-                              // Chama a função para buscar os pacientes
-                              $consult = new Login();
-                              $consultaConsultas = $consult->listarConsultas();
-
-                              // Verifica se existem registros retornados
-                              if (!empty($consultaConsultas)) {
-                                // Itera sobre os registros e exibe na tabela
-                                while ($row = $consultaConsultas->fetch_assoc()) {
-                                  echo "<tr>";
-                                  echo "<td>
-                                    <div class='d-flex align-items-center gap-2'>
-                                      <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["nome"] .
-                                    "</span>
-                                    </div>
-                                   </td>";
-                                  echo "<td>
-                                   <div class='d-flex align-items-center gap-2'>
-                                     <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["cpf"] .
-                                    "</span>
-                                   </div>
-                                  </td>";
-                                  echo "<td>
-                                    <div class='d-flex align-items-center gap-3'>
-                                      <span class='badge bg-secondary rounded-2 fw-semibold'>"
-                                    . $row["sexo"] .
-                                    "</span>
-                                    </div>
-                                   </td>";
-                                  echo "<td>
-                                   <div class='d-flex align-items-center gap-2'>
-                                     <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["telefone"] .
-                                    "</span>
-                                   </div>
-                                  </td>";
-                                  echo "<td>
-                                    <div class='d-flex align-items-center gap-2'>
-                                      <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["dt_consulta"] .
-                                    "</span>
-                                    </div>
-                                   </td>";
-
-                                  echo "<td>
-                                    <div class='d-flex align-items-center gap-2'>
-                                      <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["nomemedico"] .
-                                    "</span>
-                                    </div>
-                                   </td>";
-                                  echo "<td>
-                                   <div class='d-flex align-items-center gap-2'>
-                                     <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["especializacao"] .
-                                    "</span>
-                                   </div>
-                                  </td>";
-                                  echo "<td>
-                                    <div class='d-flex align-items-center gap-2'>
-                                      <span class='badge bg-secondary rounded-3 fw-semibold'>"
-                                    . $row["unidade_op"] .
-                                    "</span>
-                                    </div>
-                                   </td>";
-                                  // Adicione aqui mais colunas conforme necessário
-                                  echo "</tr>";
-                                }
-                              } else {
-                                echo "<tr><td colspan='4'>Nenhum registro encontrado.</td></tr>";
-                              }
-                              ?>
-                            </tr>
-                            <tr>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0">1</h6>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-1">Sunil Joshi</h6>
-                                <span class="fw-normal">Web Designer</span>
-                              </td>
-                              <td class="border-bottom-0">
-                                <p class="mb-0 fw-normal">Elite Admin</p>
-                              </td>
-                              <td class="border-bottom-0">
-                                <div class="d-flex align-items-center gap-2">
-                                  <span class="badge bg-primary rounded-3 fw-semibold">Low</span>
-                                </div>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0 fs-4">$3.9</h6>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0">2</h6>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-1">Andrew McDownland</h6>
-                                <span class="fw-normal">Project Manager</span>
-                              </td>
-                              <td class="border-bottom-0">
-                                <p class="mb-0 fw-normal">Real Homes WP Theme</p>
-                              </td>
-                              <td class="border-bottom-0">
-                                <div class="d-flex align-items-center gap-2">
-                                  <span class="badge bg-secondary rounded-3 fw-semibold">Medium</span>
-                                </div>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0 fs-4">$24.5k</h6>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0">3</h6>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-1">Christopher Jamil</h6>
-                                <span class="fw-normal">Project Manager</span>
-                              </td>
-                              <td class="border-bottom-0">
-                                <p class="mb-0 fw-normal">MedicalPro WP Theme</p>
-                              </td>
-                              <td class="border-bottom-0">
-                                <div class="d-flex align-items-center gap-2">
-                                  <span class="badge bg-danger rounded-3 fw-semibold">High</span>
-                                </div>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0 fs-4">$12.8k</h6>
-                              </td>
-                            </tr>
-
-                            <tr>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0">4</h6>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-1">Nirav Joshi</h6>
-                                <span class="fw-normal">Frontend Engineer</span>
-                              </td>
-                              <td class="border-bottom-0">
-                                <p class="mb-0 fw-normal">Hosting Press HTML</p>
-                              </td>
-                              <td class="border-bottom-0">
-                                <div class="d-flex align-items-center gap-2">
-                                  <span class="badge bg-success rounded-3 fw-semibold">Critical</span>
-                                </div>
-                              </td>
-                              <td class="border-bottom-0">
-                                <h6 class="fw-semibold mb-0 fs-4">$2.4k</h6>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-
+              <table class="table  mb-0 align-middle">
+                <thead class="text-dark fs-4">
+                  <tr>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Paciente</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 fw-semibold'>CPF</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Sexo</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Fone Paciente</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Data consulta</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Nome Medico</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Especialização</span></h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0"><span class='badge bg-dark rounded-3 '>Hospital</span></h6>
+                    </th>
+                  </tr>
+                </thead>
                 <tbody>
                   <tr>
                     <?php
@@ -451,15 +277,63 @@ if (isset($_POST['editar'])) {
                       // Itera sobre os registros e exibe na tabela
                       while ($row = $consultaConsultas->fetch_assoc()) {
                         echo "<tr>";
-                        echo "<td>" . $row["nome"] . "</td>";
-                        echo "<td>" . $row["cpf"] . "</td>";
-                        echo "<td>" . $row["sexo"] . "</td>";
-                        echo "<td>" . $row["telefone"] . "</td>";
-                        echo "<td>" . $row["dt_consulta"] . "</td>";
-                        echo "<td>" . $row["crm"] . "</td>";
-                        echo "<td>" . $row["nomemedico"] . "</td>";
-                        echo "<td>" . $row["especializacao"] . "</td>";
-                        echo "<td>" . $row["unidade_op"] . "</td>";
+                        echo "<td>
+                                    <div class='d-flex align-items-center gap-2'>
+                                      <span class='badge bg-primary rounded-3 fw-semibold'>"
+                          . $row["nome"] .
+                          "</span>
+                                    </div>
+                                   </td>";
+                        echo "<td>
+                                   <div class='d-flex align-items-center gap-2'>
+                                     <span class='badge bg-primary rounded-3 fw-semibold'>"
+                          . $row["cpf"] .
+                          "</span>
+                                   </div>
+                                  </td>";
+                        echo "<td>
+                                    <div class='d-flex align-items-center gap-3'>
+                                      <span class='badge bg-primary rounded-2 fw-semibold'>"
+                          . $row["sexo"] .
+                          "</span>
+                                    </div>
+                                   </td>";
+                        echo "<td>
+                                   <div class='d-flex align-items-center gap-2'>
+                                     <span class='badge bg-primary rounded-3 fw-semibold'>"
+                          . $row["telefone"] .
+                          "</span>
+                                   </div>
+                                  </td>";
+                        echo "<td>
+                                    <div class='d-flex align-items-center gap-2'>
+                                      <span class='badge bg-danger rounded-3 fw-semibold'>"
+                          . $row["dt_consulta"] .
+                          "</span>
+                                    </div>
+                                   </td>";
+
+                        echo "<td>
+                                    <div class='d-flex align-items-center gap-2'>
+                                      <span class='badge bg-warning rounded-3 fw-semibold'>"
+                          . $row["nomemedico"] .
+                          "</span>
+                                    </div>
+                                   </td>";
+                        echo "<td>
+                                   <div class='d-flex align-items-center gap-2'>
+                                     <span class='badge bg-warning rounded-3 fw-semibold'>"
+                          . $row["especializacao"] .
+                          "</span>
+                                   </div>
+                                  </td>";
+                        echo "<td>
+                                    <div class='d-flex align-items-center gap-2'>
+                                      <span class='badge bg-warning rounded-3 fw-semibold'>"
+                          . $row["unidade_op"] .
+                          "</span>
+                                    </div>
+                                   </td>";
                         // Adicione aqui mais colunas conforme necessário
                         echo "</tr>";
                       }
@@ -468,9 +342,15 @@ if (isset($_POST['editar'])) {
                     }
                     ?>
                   </tr>
+
+
+
+
+
                 </tbody>
               </table>
             </div>
+
           </div>
         </section>
       </main>
